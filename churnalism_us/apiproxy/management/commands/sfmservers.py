35b1c87b515a2455302b14418da0169db4fc8d5f
Drew Vogel <dvogel@sunlightfoundation.com>
2012-04-24 11:55:21 -0400

    Changed sfmservers mgmt cmd to recognize federated client.

--- churnalism_us/apiproxy/management/commands/sfmservers.py
+++ churnalism_us/apiproxy/management/commands/sfmservers.py
@@ -2,6 +2,7 @@ import socket
 from django.core.management.base import NoArgsCommand
 from django.conf import settings
 import superfastmatch
+from superfastmatch.djangoclient import from_django_conf
 
 class Command(NoArgsCommand):
     help = 'Prints details about the superfastmatch servers specified in the configuration.'
@@ -9,15 +10,24 @@ class Command(NoArgsCommand):
 
     def handle(self, *args, **options):
         for (key, cfg) in settings.SUPERFASTMATCH.iteritems():
-            print key
-            print '  url: {0}'.format(cfg['url'])
+            sfm = from_django_conf(key)
             try:
-                sfm = superfastmatch.DjangoClient(key, parse_response=True)
-                documents = sfm.documents()
-                if documents['success'] == True:
-                    print '  documents: {0}'.format(documents['total'])
+                if isinstance(sfm, superfastmatch.federated.FederatedClient):
+                    print "{0} (federated)".format(key)
+                    for (doctypes, client) in sfm.clients().iteritems():
+                        print "  doctypes: {0}".format(doctypes.replace(':', ', '))
+                        print "    url: {0}".format(client.url)
+                        documents = client.documents(doctype=doctypes, order_by='docid', limit=1)
+                        print "    documents: {0}".format(documents['total'])
                 else:
-                    print '  Unable to query for documents.'
+                    print "{0}".format(key)
+                    print '  url: {0}'.format(sfm.url)
+
+                    documents = sfm.documents()
+                    if documents['success'] == True:
+                        print '  documents: {0}'.format(documents['total'])
+                    else:
+                        print '  Unable to query for documents.'
             except (superfastmatch.SuperFastMatchError, socket.error) as e:
                 print '  Unable to query for documents: {0}'.format(str(e))
 
