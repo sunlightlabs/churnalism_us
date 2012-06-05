d244f244b39e578b137972c3ee36981c0c38e08b
Drew Vogel <dvogel@sunlightfoundation.com>
2012-06-05 15:16:17 -0400

    Integrated minimum_coverage_chars into churnstats and made churnstat tags link to permalink pages.

--- churnalism_us/apiproxy/templatetags/churnstats.py
+++ churnalism_us/apiproxy/templatetags/churnstats.py
@@ -1,6 +1,7 @@
 from apiproxy.models import SearchDocument, Match
 from django import template
 from django.conf import settings
+from django.db.models import Q
 
 register = template.Library()
 
@@ -11,13 +12,18 @@ def latest(number_latest):
     t = template.loader.get_template('apiproxy/latest_churns.html')
     recent_matches = (Match.objects
                       .filter(percent_churned__gte=settings.SIDEBYSIDE.get('minimum_coverage_pct', 0))
+                      .filter(overlapping_characters__gte=settings.SIDEBYSIDE.get('minimum_coverage_chars', 0))
+                      .filter(~Q(search_document__url=''))
+                      .filter(~Q(search_document__title=''))
                       .order_by('-percent_churned')[:20])
     for rm in recent_matches:
         if rm.search_document_id not in dedupe and len(churns) < number_latest:
             churns.append({'percent': rm.percent_churned, 
                            'title': rm.search_document.title, 
                            'text': rm.search_document.text, 
-                           'uuid': rm.search_document.uuid})
+                           'uuid': rm.search_document.uuid,
+                           'doctype': rm.matched_document.doc_type,
+                           'docid': rm.matched_document.doc_id})
             dedupe.append(rm.search_document_id)
 
     return t.render(template.Context({'latest': churns }))
@@ -27,15 +33,20 @@ def most_read(number_viewed):
     t = template.loader.get_template('apiproxy/most_viewed.html')
     matches = (Match.objects
                .filter(percent_churned__gte=settings.SIDEBYSIDE.get('minimum_coverage_pct', 0))
+               .filter(overlapping_characters__gte=settings.SIDEBYSIDE.get('minimum_coverage_chars', 0))
+               .filter(~Q(search_document__url=''))
+               .filter(~Q(search_document__title=''))
                .order_by('-number_matches')[:20])
     churns = []
     dedupe = []
     for m in matches:
-        if m.search_document_id not in dedupe and len(churns) < number_viewed :
+        if m.search_document_id not in dedupe and len(churns) < number_viewed:
             churns.append({'percent': m.percent_churned, 
                            'title':m.search_document.title, 
                            'text': m.search_document.text, 
-                           'uuid': m.search_document.uuid})
+                           'uuid': m.search_document.uuid,
+                           'doctype': m.matched_document.doc_type,
+                           'docid': m.matched_document.doc_id})
             dedupe.append(m.search_document_id)
 
     return t.render(template.Context({'viewed': churns}))
