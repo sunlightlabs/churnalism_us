96037fa6b1cca0a4f801f8ab71dc877eafab64e8
Drew Vogel <dvogel@sunlightfoundation.com>
2012-07-27 14:53:20 -0400

    Let requests decide which encoding to use.

--- churnalism_us/utils/slurp_url.py
+++ churnalism_us/utils/slurp_url.py
@@ -10,7 +10,7 @@ def slurp_url(url, use_cache=False):
     def _slurp_url(url):
         resp = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.21 (KHTML, like Gecko) Chrome/19.0.1041.0 Safari/535.21'}, config={'verbose': sys.stderr})
         if resp.status_code == 200:
-            return resp.content.decode('utf-8', 'ignore')
+            return resp.text
         else:
             logging.error('Unexpected status ({status}) for ({url})'.format(url=url, status=resp.status_code))
             return None
